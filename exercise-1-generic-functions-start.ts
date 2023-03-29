e42230f78379c50767b1aba08b04e91a38540251
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-03-29 16:51:52 +0200

    nuovo

---
+++ exercise-1-generic-functions-start.ts
@@ -0,0 +1,37 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Make this identity function generic.
+// This should fix the type errors on Line 13 and 15.
+// Hint: The type for the `value` function parameter should use a type variable.
+
+function identity<type>(value:type):type {
+    return value;
+}
+
+let value1 = identity<string>("France");
+
+let value2 = identity<number>(67_413_000);
+
+// ----
+
+async function fetchData<Type>(url: string) {
+    const response = await fetch(url);
+    const responseBody = await response.json();
+
+    return responseBody as Type;
+}
+
+interface User {
+    name: string;
+}
+let user: User = await fetchData<User>("http://api.com/user/1");
+// Pass the `User` type as a type argument to the generic `fetchData()` function.
+// This should fix the type error on Line 35.
+
+console.log(user.name);
+
+// ----
+
+export {};
